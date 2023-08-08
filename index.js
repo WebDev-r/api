797599ea658477a6fba901b601c273f0b71783c2
jitendra singh <js8322870@gmail.com>
2023-08-08 20:37:26 +0530

    api

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app=express()
+const PORT=process.env.PORT || 3000
+app.get('/',(req,resp)=>{
+    console.log("get is called");
+    resp.send("get is called")
+})
+app.post('/',(req,resp)=>{
+    console.log("post is called");
+    resp.send("post is called")
+})
+app.listen(PORT,()=>{
+    console.log("server started")
+})
